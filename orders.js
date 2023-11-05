ea96d39f502779e12f9c0bed5e72d1acc9f343bd
adnane <adnanedu2016@gmail.com>
2023-11-05 11:29:21 +0000

    initial commit

---
+++ orders.js
@@ -0,0 +1,34 @@
+const Orders = [
+    {
+        productName:'Appel Watch',
+        productNumber:'75148',
+        paymentStatus:'NY',
+        status:'Declined',
+    },
+    {
+        productName:'Smart Tv',
+        productNumber:'32898',
+        paymentStatus:'Paid',
+        status:'Pending',
+    },
+    {
+        productName:'Appel Watch',
+        productNumber:'13122',
+        paymentStatus:'Paid',
+        status:'Active',
+    },
+    {
+        productName:'Mac Air 2024',
+        productNumber:'52127',
+        paymentStatus:'Paid',
+        status:'Pending',
+    },
+    {
+        productName:'Air Tag',
+        productNumber:'30540',
+        paymentStatus:'NY',
+        status:'Declined',
+    },
+   
+ 
+]
